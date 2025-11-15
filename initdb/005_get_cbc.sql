dda89391134f553444098c76a895624aee9d85f9
strelcov.georgiy <strelcov.georgiy@rwb.ru>
2025-11-16 02:40:03 +0300

    + Фикс get_cbc

--- initdb/005_get_cbc.sql
+++ initdb/005_get_cbc.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION dataset.get_cbc(
     _dataset_id INT,
-    _gender SMALLINT DEFAULT NULL   -
+    _gender SMALLINT DEFAULT NULL
 )
 RETURNS JSONB
 LANGUAGE plpgsql
@@ -8,13 +8,22 @@ AS
 $$
 DECLARE
 _result JSONB;
+    _info JSONB;
 BEGIN
+SELECT to_jsonb(m)
+INTO _info
+FROM dataset.dataset_meta m
+WHERE m.dataset_id = _dataset_id;
+
 SELECT jsonb_agg(to_jsonb(c))
 INTO _result
 FROM dataset.cbc c
 WHERE c.dataset_id = _dataset_id
   AND (_gender IS NULL OR c.gender = _gender);
 
-RETURN COALESCE(_result, '[]'::JSONB);
+RETURN jsonb_build_object(
+        'rows', COALESCE(_result, '[]'::JSONB),
+        'info', _info
+       );
 END;
 $$;
